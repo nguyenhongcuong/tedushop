649b2eb415cc349bf08a60eda61ebf5b9911d0e6
Nguyen Hong Cuong <nguyenhongcuong69@gmail.com>
2019-06-01 19:56:17 +0700

    add unit test for repository and service

---
+++ TeduShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        private IDbFactory _dbFactory;
+        private IUnitOfWork _unitOfWork;
+        private IPostCategoryRepository _repository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _dbFactory = new DbFactory();
+            _repository = new PostCategoryRepository(_dbFactory);
+            _unitOfWork = new UnitOfWork(_dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory postCategory = new PostCategory
+            {
+                Name = "Test category",
+                Alias = "test-category",
+                Status = true
+            };
+            PostCategory result = _repository.Add(postCategory);
+            _unitOfWork.Commit();
+
+            Assert.IsTrue(result.Id > 0);
+
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            System.Collections.Generic.IEnumerable<PostCategory> result = _repository.GetAll();
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ TeduShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+using TeduShop.Service.Services;
+
+namespace TeduShop.UnitTest.ServiceTest
+{
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> _mockRepository;
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        private IPostCategoryService _categoryService;
+        private List<PostCategory> _postCategories;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IPostCategoryRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _categoryService = new PostCategoryService(_mockUnitOfWork.Object, _mockRepository.Object);
+            _postCategories = new List<PostCategory>
+            {
+                new PostCategory{Id = 1, Name = "Danh mục 1", Alias = "danh-muc-1", Status = true}
+            };
+        }
+
+        [TestMethod]
+        public void PostCateogry_Service_GetAll()
+        {
+            // setup method
+            _mockRepository.Setup(m => m.GetAll(null)).Returns(_postCategories);
+
+            // call action
+            IEnumerable<PostCategory> result = _categoryService.Get();
+
+            // compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void PostCateogry_Service_Create()
+        {
+            PostCategory postCategory = new PostCategory
+            {
+                Name = "Test Category",
+                Alias = "test-category",
+                Status = true
+            };
+
+            _mockRepository.Setup(m => m.Add(postCategory)).Returns((PostCategory p) =>
+            {
+                p.Id = 1;
+                return p;
+            });
+
+            PostCategory result = _categoryService.Add(postCategory);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Id);
+        }
+    }
+}
